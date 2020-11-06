5f74579be165157db4ccdf886a6506714286a4e0
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-11-06 17:51:57 +0530

    Changes to task table to incorporate completed tasks

--- src/Components/TaskTable/TaskTable.tsx
+++ src/Components/TaskTable/TaskTable.tsx
@@ -1,14 +1,16 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { Table, Button } from 'antd';
+import { Table, Button, notification } from 'antd';
 import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
+
+import CompletedTable from "../CompletedTable/CompletedTable";
 
 import "./TaskTable.less";
 
 const TaskTable: React.FC<TaskTableProps> = (props) => {
 
-    //const { data } = props;
-
     const [data, setData] = useState(props);
+
+    const [completed, setCompleted] = useState<TaskTableProps[]>([]);
 
     useEffect(() => {
 
@@ -22,13 +24,36 @@ const TaskTable: React.FC<TaskTableProps> = (props) => {
 
         setData(spliced)
 
+        notification.error({
+            message: "Deleted",
+            description: "Task Successfully Deleted"
+        });
+
     }, [data]);
+
+    const onFinish = useCallback((record: any, index: any) => {
+
+        record.status = "Done"
+
+        setCompleted([...completed, record]);
+
+        notification.success({
+            message: "Completed",
+            description: "Task Completed"
+        });
+
+        const spliced = data.data.splice(index, 1);
+
+        setData(spliced)
+
+    }, [completed, data]);
 
     const columns = [
         {
             title: 'Task',
             dataIndex: 'task',
             key: 'task',
+            width: '600px',
             render: (text: any) => <a>{text}</a>,
         },
         {
@@ -39,6 +64,7 @@ const TaskTable: React.FC<TaskTableProps> = (props) => {
         {
             title: 'Action',
             key: 'action',
+            width: '120px',
             render: (text: any, record: any, index: any) => (
                 <>
                     <Button
@@ -46,6 +72,7 @@ const TaskTable: React.FC<TaskTableProps> = (props) => {
                         shape={"circle"}
                         type={"primary"}
                         style={{ marginRight: 8 }}
+                        onClick={() => onFinish(record, index)}
                     />
                     <Button
                         icon={<CloseOutlined />}
@@ -54,7 +81,7 @@ const TaskTable: React.FC<TaskTableProps> = (props) => {
                     />
                 </>
             ),
-        },
+        }
     ];
 
     return (
@@ -67,6 +94,14 @@ const TaskTable: React.FC<TaskTableProps> = (props) => {
                 dataSource={data.data}
             />
 
+            {completed.length > 0 &&
+                <div className="complete-container">
+                    <CompletedTable
+                        data={completed}
+                    />
+                </div>
+
+            }
 
         </div>
 
@@ -78,3 +113,5 @@ interface TaskTableProps {
 }
 
 export default TaskTable;
+
+
